836eaac83b2dcad2133e08ecae68db5feca5accf
LEGACY-OF-JUANC\Usuario <juansim87@gmail.com>
2025-09-03 13:24:30 +0200

    feat(App): start Landing Page Design System Practice project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export const App = () => {
+
+  return (
+    <div className="min-h-[100vh] flex flex-column justify-center items-center">
+     <h1 className='text-[80px] text-red-600'>Hola</h1>
+    </div>
+  )
+};
